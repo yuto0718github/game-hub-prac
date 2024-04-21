1268e44b4595bdc7ea8ace425812b6b8479e7961
Yuto <yuto.y.0718@gmail.com>
2024-04-20 21:25:57 -0700

    Refactor grid layout and add Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -11,12 +12,12 @@ function App() {
         `,
           lg: `
           "nav nav"
-          "main aside"
+          "aside main"
         `,
         }}
       >
-        <GridItem area="nav" bg="coral">
-          Nav
+        <GridItem area="nav">
+          <Navbar></Navbar>
         </GridItem>
         <Show above="lg">
           <GridItem area="aside" bg="gold">
