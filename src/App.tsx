0a29b94994d545d79d43db7f2bf97fe610b1b82b
Yuto <yuto.y.0718@gmail.com>
2024-04-20 21:13:54 -0700

    Refactor App component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button>Chakra</Button>
+      <Grid
+        templateAreas={{
+          base: `
+          "nav"
+          "main"
+        `,
+          lg: `
+          "nav nav"
+          "main aside"
+        `,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
