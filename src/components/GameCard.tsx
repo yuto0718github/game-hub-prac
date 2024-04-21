7d8c8c1f2371bee4c36eba2f49f5800ac2490faa
Yuto <yuto.y.0718@gmail.com>
2024-04-21 01:08:31 -0700

    Add react-icons package and display platform icons on GameCard component

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
-import { Card, Image, CardBody, Heading } from "@chakra-ui/react";
+import { Card, Image, CardBody, Heading, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import { PlatformIconList } from "./PlatformIconList";
 
 interface GameCardProps {
   game: Game;
@@ -11,6 +12,9 @@ export const GameCard = ({ game }: GameCardProps) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        ></PlatformIconList>
       </CardBody>
     </Card>
   );
---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,41 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { Platform } from "../hooks/useGames";
+
+interface PlatformIconListProps {
+  platforms: Platform[];
+}
+
+export const PlatformIconList = ({ platforms }: PlatformIconListProps) => {
+  const iconMap: { [key: string] } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    ios: MdPhoneIphone,
+    mac: FaApple,
+    linux: FaLinux,
+    andoroid: FaAndroid,
+    nintendo: SiNintendo,
+    web: BsGlobe,
+  };
+
+  return (
+    <>
+      <HStack marginY={1}>
+        {platforms.map((platform) => (
+          <Icon as={iconMap[platform.slug]} color="gray.500" />
+        ))}
+      </HStack>
+    </>
+  );
+};
