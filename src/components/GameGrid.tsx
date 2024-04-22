572d7370ec574d8ff8e2ed0d98802cadb58db4b1
Yuto <yuto.y.0718@gmail.com>
2024-04-22 15:51:31 -0700

    Update GameCard and GameGrid components

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,9 +2,11 @@ import { Text } from "chakra-ui/react";
 import useGames from "../hooks/useGames";
 import { SimpleGrid } from "@chakra-ui/react";
 import { GameCard } from "./Gamecard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
@@ -14,6 +16,8 @@ const GameGrid = () => {
         padding="10px"
         spacing="10"
       >
+        {isLoading &&
+          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
