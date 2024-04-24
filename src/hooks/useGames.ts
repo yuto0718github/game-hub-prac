46bc30818346e4cde027b9384eeaffb6347eeef7
Yuto <yuto.y.0718@gmail.com>
2024-04-24 16:10:12 -0700

    Refactor App component to use GameQuery object

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,6 @@
 import useData from "./useData";
 import { Genre } from "./useGenres";
+import { GameQuery } from "../App";
 
 export interface Platform {
     id: number;
@@ -15,13 +16,13 @@ export interface Game {
   }
 
   
-const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => 
+const useGames = (gameQuery : GameQuery) => 
 useData<Game>("/games", {
     params: {
-        genres:selectedGenre?.id, 
-        platforms:selectedPlatform?.id}
+        genres:gameQuery.genre?.id, 
+        platforms:gameQuery.platform?.id}
     }, 
-    [selectedGenre?.id, selectedPlatform?.id]);
+    [gameQuery]);
 
 
 
